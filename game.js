a8c9f708469f0046076184c8b18059abe924d74c
pepsin <pepsin>
2018-06-27 07:19:29 +0800

    Sprint Action

--- game.js
+++ game.js
@@ -19,8 +19,10 @@ stage.add(player.bulletGroup)
 
 let touchX = null
 let touchY = null
+let isShoot = false
 
 wx.onTouchStart(function (e) {
+	isShoot = false
   touchX = e.touches[0].clientX
   touchY = e.touches[0].clientY
 })
@@ -30,14 +32,22 @@ wx.onTouchMove(function (e) {
   touchY = e.touches[0].clientY
 })
 
+wx.onTouchEnd(function (e) {
+	isShoot = true
+})
+
 function update () {
   stage.update()
   bg.update()
 
   player.update()
   if (touchX !== null) {
-    player.x = touchX
-    player.y = touchY
+		if (isShoot) {
+			player.isShoot = isShoot
+		} else {
+	    player.x = touchX
+	    player.y = touchY
+		}		
   }
   enemyGroup.update()
 
@@ -55,16 +65,3 @@ function update () {
 }
 
 update()
-
-
-const text = new cax.Text('powered by cax', {
-    font: '20px Arial',
-    color: '#42B035',
-    baseline: 'top'
-  })
-text.y = screenHeight - 30
-text.x = 4
-text.alpha = 0.6
-stage.add(text)
-
-cax.To.get(text).to().x(100, 2000, cax.easing.elasticInOut).start()
--- js/player.js
+++ js/player.js
@@ -27,27 +27,54 @@ export default class Player extends cax.Group {
 
     this.preShootTime = Date.now()
     this.bulletGroup = new cax.Group()
+		this.isShoot = false
+		this.frames = 0
+		this.speedX = 0
+		this.speedY = 0
+		this.shootAngle = 0
   }
 
   update () {
     this.currentTime = Date.now()
     if (this.currentTime - this.preShootTime > 200) {
-      this.shoot()
+      // this.shoot()
       this.preShootTime = this.currentTime
     }
 
-    this.bulletGroup.children.forEach(bullet => {
-      bullet.update()
-    })
+    // this.bulletGroup.children.forEach(bullet => {
+    //   bullet.update()
+    // })
+			
+			if (this.isShoot) {
+				if (this.shootAngle == 0) {
+					var originX = screenWidth / 2
+					var originY = screenHeight - 80
+					var theta = Math.atan((originY - this.y) / (originX - this.x))
+					if (originX - this.x < 0) {
+						theta = Math.PI + theta
+					}
+					this.shootAngle = theta
+				}
+				
+				this.frames += 1
+				var seconds = this.frames / 60
+				var speed = 10
+				var gravity = 10
+				this.speedX = Math.cos(this.shootAngle) * speed
+				this.speedY = Math.sin(this.shootAngle) * speed
+				console.log(this.speedX, this.speedY)
+				this.x += this.speedX
+        this.y += (this.speedY + (gravity * seconds))
+			}
   }
 
-  shoot () {
-    let bullet = new Bullet()
-    bullet.x = this.x
-    bullet.y = this.y - 30
-    this.music.playShoot()
-    this.bulletGroup.add(bullet)
-  }
+  // shoot () {
+  //   let bullet = new Bullet()
+  //   bullet.x = this.x
+  //   bullet.y = this.y - 30
+  //   this.music.playShoot()
+  //   this.bulletGroup.add(bullet)
+  // }
 
   isCollideWith (sp) {
     let spX = sp.x + sp.width / 2
